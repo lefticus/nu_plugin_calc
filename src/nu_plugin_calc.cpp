7f94960c957b58f567a4e17f2bb46cb4e7e3bfc3
Jason Turner <jason@emptycrate.com>
2019-09-26 11:06:11 -0600

    Add logger support

--- src/nu_plugin_calc.cpp
+++ src/nu_plugin_calc.cpp
@@ -1,12 +1,13 @@
-// TODO: Implement Me
-//
-//
 #include <string_view>
-#include <nlohmann/json.hpp>
-#include <fmt/format.h>
 #include <iostream>
 #include <regex>
 #include <sstream>
+
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 void print_good_response(const nlohmann::json &response)
 {
@@ -18,29 +19,59 @@ void print_good_response(const nlohmann::json &response)
 
 struct Calculator
 {
+  Calculator(spdlog::logger &logger) : m_logger{ logger } {}
+
+  std::reference_wrapper<spdlog::logger> m_logger;
   std::map<std::string, double> variables;
+
 
   static std::regex make_regex()
   {
     const std::string_view floating_point = R"(([+-]?([0-9]*[.])?[0-9]+))";
     const std::string_view operators = R"(([-*/+]))";
     const std::string_view identifier = R"(([a-z]+))";
-    return std::regex{ fmt::format(R"(\s*({}\s*=\s*)?({}|{})\s*{}\s*({}|{})\s*)",
-      identifier,
-      floating_point,
-      identifier,
-      operators,
-      floating_point,
-      identifier) };
+
+    return std::regex{ fmt::format(R"(\s*({id}\s*=\s*)?({fp}|{id})\s*{op}\s*({fp}|{id})\s*)",
+      fmt::arg("id", identifier),
+      fmt::arg("fp", floating_point),
+      fmt::arg("op", operators)) };
+  }
+
+  enum Regex_Matches {
+    Result_Identifier = 2,
+    Param_1 = 3,
+    Param_1_Number = 4,
+    Param_1_Identifier = 6,
+    Operation = 7,
+    Param_2 = 8,
+    Param_2_Number = 9,
+    Param_2_Identifier = 11,
+  };
+
+  std::regex expression_regex = make_regex();
+
+  auto evaluate_expression(const char operation, const double lhs, const double rhs)
+  {
+    switch (operation) {
+    case '-':
+      return lhs - rhs;
+    case '+':
+      return lhs + rhs;
+    case '*':
+      return lhs * rhs;
+    case '/':
+      return lhs / rhs;
+    default:
+      m_logger.get().error("Unknown operator! '{}'", operation);
+      return 0.0;
+    }
   }
 
   auto calculate(const nlohmann::json &string_value)
   {
-    const static std::regex regex{ make_regex() };
+    const auto string = static_cast<std::string>(string_value["item"]["Primitive"]["String"]);
 
-    const std::string string = string_value["item"]["Primitive"]["String"];
-
-    const auto get_value = [&](const auto &value, const auto &identifier) {
+    const auto get_parameter_value = [&](const auto &value, const auto &identifier) {
       if (identifier.matched) {
         return variables[identifier];
       } else {
@@ -49,34 +80,35 @@ struct Calculator
     };
 
     const auto result = [&]() {
-      if (std::smatch results; std::regex_match(string, results, regex)) {
-        for (const auto &match : results) { std::cerr << "Result: '" << match << "'\n"; }
+      if (std::smatch results; std::regex_match(string, results, expression_regex)) {
+        for (const auto &match : results) { m_logger.get().trace("Regex result: '{}'", match.str()); }
 
-        const auto param1 = get_value(results[4], results[6]);
-        const auto operation = *results[7].first;
-        const auto param2 = get_value(results[9], results[11]);
+        const auto lhs = get_parameter_value(results[Param_1_Number], results[Param_1_Identifier]);
+        const auto operation = *results[Operation].first;
+        const auto rhs = get_parameter_value(results[Param_2_Number], results[Param_2_Identifier]);
 
-        const auto value = [&]() {
-          switch (operation) {
-          case '-':
-            return param1 - param2;
-          case '+':
-            return param1 + param2;
-          case '*':
-            return param1 * param2;
-          case '/':
-            return param1 / param2;
-          }
-        }();
 
-        if (results[2].matched) { variables[results[2]] = value; }
+        const auto expression_result = evaluate_expression(operation, lhs, rhs);
 
-        return value;
+        if (results[Result_Identifier].matched) { variables[results[Result_Identifier]] = expression_result; }
+
+        m_logger.get().info("Parsed: '{}' = '{}' '{}' '{}'; Evaluated '{} {} {}' = {}",
+          results[Result_Identifier].str(),
+          results[Param_1].str(),
+          operation,
+          results[Param_2].str(),
+          lhs,
+          operation,
+          rhs,
+          expression_result);
+
+
+        return expression_result;
       } else {
+        m_logger.get().info("No expression parsed");
         return 0.0;
       }
     }();
-
 
     auto int_value = string_value;
     int_value["item"] = { { "Primitive", { { "Decimal", result } } } };
@@ -86,10 +118,15 @@ struct Calculator
 
 int main()
 {
-  Calculator calculator;
+  auto console_logger{ spdlog::stderr_color_mt("console_logger") };
+  console_logger->set_level(spdlog::level::info);
+  console_logger->set_pattern("[%Y-%m-%d %H:%M:%S %z] [%n] [%^%l%$] %v");
+  console_logger->info("nu_plugin_calc_starting");
+
+  Calculator calculator{ *console_logger };
 
   for (std::string line; std::cin.good(); std::getline(std::cin, line)) {
-    //    std::cerr << "Trying to parse: '" << line << '\'' << std::endl;
+    console_logger->trace("Trying to parse: '{}'", line);
 
     if (line.empty()) { continue; }
 
