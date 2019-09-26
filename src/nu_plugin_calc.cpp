0a67722d0e420df1e29d3272a2f08e83421cebd7
Jason Turner <jason@emptycrate.com>
2019-09-25 23:11:24 -0600

    Basic expression parser

--- src/nu_plugin_calc.cpp
+++ src/nu_plugin_calc.cpp
@@ -5,6 +5,7 @@
 #include <nlohmann/json.hpp>
 #include <fmt/format.h>
 #include <iostream>
+#include <regex>
 #include <sstream>
 
 void print_good_response(const nlohmann::json &response)
@@ -15,17 +16,78 @@ void print_good_response(const nlohmann::json &response)
   puts(json_response.dump().c_str());
 }
 
-auto get_length(const nlohmann::json &string_value)
+struct Calculator
 {
-  const std::string string = string_value["item"]["Primitive"]["String"];
-  const auto string_len = string.size();
-  auto int_value = string_value;
-  int_value["item"] = { { "Primitive", { { "Int", string_len } } } };
-  return int_value;
-}
+  std::map<std::string, double> variables;
+
+  static std::regex make_regex()
+  {
+    const std::string_view floating_point = R"(([+-]?([0-9]*[.])?[0-9]+))";
+    const std::string_view operators = R"(([-*/+]))";
+    const std::string_view identifier = R"(([a-z]+))";
+    return std::regex{ fmt::format(R"(\s*({}\s*=\s*)?({}|{})\s*{}\s*({}|{})\s*)",
+      identifier,
+      floating_point,
+      identifier,
+      operators,
+      floating_point,
+      identifier) };
+  }
+
+  auto calculate(const nlohmann::json &string_value)
+  {
+    const static std::regex regex{ make_regex() };
+
+    const std::string string = string_value["item"]["Primitive"]["String"];
+
+    const auto get_value = [&](const auto &value, const auto &identifier) {
+      if (identifier.matched) {
+        return variables[identifier];
+      } else {
+        return std::stod(value);
+      }
+    };
+
+    const auto result = [&]() {
+      if (std::smatch results; std::regex_match(string, results, regex)) {
+        for (const auto &match : results) { std::cerr << "Result: '" << match << "'\n"; }
+
+        const auto param1 = get_value(results[4], results[6]);
+        const auto operation = *results[7].first;
+        const auto param2 = get_value(results[9], results[11]);
+
+        const auto value = [&]() {
+          switch (operation) {
+          case '-':
+            return param1 - param2;
+          case '+':
+            return param1 + param2;
+          case '*':
+            return param1 * param2;
+          case '/':
+            return param1 / param2;
+          }
+        }();
+
+        if (results[2].matched) { variables[results[2]] = value; }
+
+        return value;
+      } else {
+        return 0.0;
+      }
+    }();
+
+
+    auto int_value = string_value;
+    int_value["item"] = { { "Primitive", { { "Decimal", result } } } };
+    return int_value;
+  }
+};
 
 int main()
 {
+  Calculator calculator;
+
   for (std::string line; std::cin.good(); std::getline(std::cin, line)) {
     //    std::cerr << "Trying to parse: '" << line << '\'' << std::endl;
 
@@ -35,7 +97,7 @@ int main()
     const auto method = x["method"];
     if (method == "config") {
       const auto config = nlohmann::json{ { "name", "calc" },
-        { "usage", "Return the length of a string" },
+        { "usage", "Performs mathematical operations separated by new lines." },
         { "positional", nlohmann::json::array() },
         { "named", nlohmann::json::object() },
         { "is_filter", true } };
@@ -44,7 +106,7 @@ int main()
       print_good_response(nlohmann::json::array());
     } else if (method == "filter") {
       const auto array =
-        nlohmann::json::array({ nlohmann::json{ { "Ok", { { "Value", get_length(x["params"]) } } } } });
+        nlohmann::json::array({ nlohmann::json{ { "Ok", { { "Value", calculator.calculate(x["params"]) } } } } });
       print_good_response(array);
     } else if (method == "end_filter") {
       print_good_response(nlohmann::json::array());
