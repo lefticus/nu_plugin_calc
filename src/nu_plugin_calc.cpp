e6e59a7c6d3a46b1024105a837dae0ed1b7a613b
Jason Turner <jason@emptycrate.com>
2019-09-25 21:51:50 -0600

    Move from parsing JSON to more idiomatic inline JSON objs

--- src/nu_plugin_calc.cpp
+++ src/nu_plugin_calc.cpp
@@ -9,48 +9,45 @@
 
 void print_good_response(const nlohmann::json &response)
 {
-  std::stringstream ss;
-  ss << R"({"jsonrpc": "2.0", "method": "response", "params": {"Ok": )" << response.dump() << "}}";
+  const auto json_response =
+    nlohmann::json{ { "jsonrpc", "2.0" }, { "method", "response" }, { "params", { { "Ok", response } } } };
 
-  puts(ss.str().c_str());
+  puts(json_response.dump().c_str());
 }
 
 auto get_length(const nlohmann::json &string_value)
 {
   const std::string string = string_value["item"]["Primitive"]["String"];
   const auto string_len = string.size();
-  std::stringstream ss;
-  ss << R"({"Primitive": {"Int": )" << string_len << "}}";
-  const auto int_item = nlohmann::json::parse(ss.str());
   auto int_value = string_value;
-  int_value["item"] = int_item;
+  int_value["item"] = { { "Primitive", { { "Int", string_len } } } };
   return int_value;
 }
 
 int main()
 {
   for (std::string line; std::cin.good(); std::getline(std::cin, line)) {
-//    std::cerr << "Trying to parse: '" << line << '\'' << std::endl;
+    //    std::cerr << "Trying to parse: '" << line << '\'' << std::endl;
 
     if (line.empty()) { continue; }
 
     const auto x = nlohmann::json::parse(line);
     const auto method = x["method"];
     if (method == "config") {
-      auto config = nlohmann::json::parse(R"({"name": "calc", "usage": "Return the length of a string", "positional": [], "named": {}, "is_filter": true})");
+      const auto config = nlohmann::json{ { "name", "calc" },
+        { "usage", "Return the length of a string" },
+        { "positional", nlohmann::json::array() },
+        { "named", nlohmann::json::object() },
+        { "is_filter", true } };
       print_good_response(config);
     } else if (method == "begin_filter") {
-      print_good_response(nlohmann::json::parse("[]"));
+      print_good_response(nlohmann::json::array());
     } else if (method == "filter") {
-      const auto int_item = get_length(x["params"]);
-      std::stringstream ss;
-      ss << R"([ { "Ok" : { "Value" : )";
-      ss << int_item;
-      ss << "}}]";
-      print_good_response(nlohmann::json::parse(ss.str()));
+      const auto array =
+        nlohmann::json::array({ nlohmann::json{ { "Ok", { { "Value", get_length(x["params"]) } } } } });
+      print_good_response(array);
     } else if (method == "end_filter") {
-      print_good_response(nlohmann::json::parse("[]"));
+      print_good_response(nlohmann::json::array());
     }
   }
 }
-
